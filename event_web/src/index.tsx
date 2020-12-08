67fe1966759095750085f19897539f84e9be8240
martin <codemcm@hotmail.com>
2020-12-07 19:26:44 -0600

    plantillas add

--- event_web/src/index.tsx
+++ event_web/src/index.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Login from './login/Login';
+import Album from './album/Album';
+import Dashboard from './dashboard/Dashboard';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Dashboard />
   </React.StrictMode>,
   document.getElementById('root')
 );
